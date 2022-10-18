ff1d32fface3213e8b4cda12a07b2049c98eb1d9
laura <laurajorques2003@gmail.com>
2022-10-18 16:12:18 +0200

    proyecto acabado

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -6,49 +6,77 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView res = (TextView) findViewById(R.id.resultado);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void resultado(View view) {
-        res.setText(calcular(res.getText().toString()));
-    }
-
     public void uno(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"1");
     }
     public void dos(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"2");
     }
     public void tres(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"3");
     }
     public void cuatro(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"4");
     }
     public void cinco(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"5");
     }
     public void seis(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"6");
     }
     public void siete(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"7");
     }
     public void ocho(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"8");
     }
     public void nueve(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"9");
     }
     public void cero(View view) {
-        res.setText(res.getText());
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"0");
+    }
+    public void mas(View view) {
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(res.getText()+"+");
+    }
+    public void c(View view) {
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText("");
+    }
+    public void resultado(View view) {
+        TextView res = (TextView) findViewById(R.id.resultado);
+        res.setText(calcular(res.getText().toString()));
     }
     public String calcular(String cadena) {
+        int mas;
+        String num1;
+        String num2;
 
+        mas = cadena.indexOf("+");
+        num1 = cadena.substring(0, (mas));
+        num2 = cadena.substring((mas)+1, cadena.length());
+        System.out.println(mas);
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+        cadena = Integer.toString(Integer.parseInt(num1)+Integer.parseInt(num2));
         return cadena;
     }
 }
